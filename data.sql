a925206c0cd1135c52a41242bbe6e4b169a33208
Anwesha <anwesha.bhar@gmail.com>
2021-01-22 16:42:10 +0530

    Added sql script for hotel db

---
+++ data.sql
@@ -0,0 +1,22 @@
+INSERT INTO Hotel_Info (hotel_Id, hotel_Name, hotel_Desc, city) VALUES
+  (1, 'Novotel', '5 star hotel with great facilities', 'Kolkata'),
+  (2, 'Ibis', '4 star hotel', 'Mumbai'),
+  (3, 'Radisson', '4 star hotel' ,'Hyderabad'),
+  (4, 'Taj', '5 star hotel', 'Mumbai');
+
+INSERT INTO Rooms (room_Id,hotel_Id, room_Type, price, max_guest_allowed ) VALUES
+  (1, 1, 'KingBed', 2000, 3),
+  (2, 1, 'DoubleQueen', 3000, 4),
+  (3, 1, 'RunOfTheHouse', 1000, 2),
+  (4, 2, 'KingBed',2000, 3),
+  (5, 2, 'DoubleQueen',3000, 4),
+  (6, 2, 'RunOfTheHouse',1000, 2),
+  (7, 3, 'KingBed', 2000, 3),
+  (8, 3, 'DoubleQueen',3000, 4),
+  (9, 3, 'RunOfTheHouse',1000, 2),
+  (10, 1, 'KingBed', 2000, 3),
+  (11, 1, 'DoubleQueen', 3000, 4),
+  (12, 1, 'RunOfTheHouse', 1000, 2),
+  (13, 4, 'KingBed', 2000, 3),
+  (14, 4, 'DoubleQueen', 3000, 4),
+  (15, 4, 'RunOfTheHouse', 1000, 2);
---
+++ schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS Hotel_Info;
+
+CREATE TABLE Hotel_Info (
+  hotel_Id int(18) PRIMARY KEY,
+  hotel_Name varchar(25) NOT NULL,
+  hotel_Desc varchar(100),
+  city varchar(25)
+);
+
+DROP TABLE IF EXISTS Rooms;
+
+CREATE TABLE Rooms (
+  room_Id int(18) PRIMARY KEY,
+  hotel_Id int(18),
+  room_Type varchar(25) NOT NULL,
+  price int(18),
+  max_guest_allowed int(18),
+  CONSTRAINT rooms_fk_1 FOREIGN KEY (hotel_Id) REFERENCES Hotel_Info (hotel_Id)
+) ;
